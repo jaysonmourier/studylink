8b3e345e141c642ffa68c6c09dbcd80e33a4ae31
Jayson MOURIER <Jayson@dphpmcf77.home>
2022-11-01 15:15:54 +0100

    add spring boot project (maven)

---
+++ studylink_api/src/main/java/com/miageapp/studylink_api/StudylinkApiApplication.java
@@ -0,0 +1,13 @@
+package com.miageapp.studylink_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudylinkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudylinkApiApplication.class, args);
+	}
+
+}
