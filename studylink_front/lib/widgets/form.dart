84af97971603664b357410bcbb6d960d76b5257a
Jayson Mourier <mourierjayson@protonmail.com>
2022-11-08 16:04:23 +0100

    updated form.dart

--- studylink_front/lib/widgets/form.dart
+++ studylink_front/lib/widgets/form.dart
@@ -17,22 +17,13 @@ class FormWidgetState extends State<FormWidget> {
     return Form(
       key: _formKey,
       child: Column(
-        children: <Widget>[
-          TextFormField(
-            decoration: const InputDecoration(
-              icon: Icon(Icons.person),
-              hintText: 'What do people call you?',
-              labelText: 'Name *',
+        children: const <Widget>[
+          TextField(
+            obscureText: false,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Content',
             ),
-            onSaved: (String? value) {
-              // This optional block of code can be used to run
-              // code when the user saves the form.
-            },
-            validator: (String? value) {
-              return (value != null && value.contains('@'))
-                  ? 'Do not use the @ char.'
-                  : null;
-            },
           )
         ],
       ),
