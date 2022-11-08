a88b0d140574dbade867405d68f27e76035b6a34
Dany ABOUADAOU <dany.abouadaou@metaline.fr>
2022-11-08 19:46:22 +0100

    Lombok dependency and refactoring

--- studylink_api/src/main/java/com/devops/studylink/post/domain/model/Post.java
+++ studylink_api/src/main/java/com/devops/studylink/post/domain/model/Post.java
@@ -2,15 +2,13 @@ package com.devops.studylink.post.domain.model;
 
 import java.util.Date;
 import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor
 public class Post {
-
-    public Post() {}
-    public Post(UUID id, String content, Date creationDate) {
-        this.id = id;
-        this.content = content;
-        this.creationDate = creationDate;
-    }
 
     private UUID id;
     private String content;
@@ -23,30 +21,6 @@ public class Post {
         // setCreationDate(update.getCreationDate()); // ?
     }
 
-    /**| SETTERS & GETTERS |**/
+    // TODO : public void create(Post)
 
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public Date getCreationDate() {
-        return creationDate;
-    }
-
-    public void setCreationDate(Date creationDate) {
-        this.creationDate = creationDate;
-    }
-    
 }
