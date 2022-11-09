b6c5ec3182ce887826c8fef138ae07f2a28f2a73
Jayson Mourier <mourierjayson@protonmail.com>
2022-11-09 14:20:27 +0100

    the form is now setup

--- studylink_front/lib/widgets/form.dart
+++ studylink_front/lib/widgets/form.dart
@@ -1,4 +1,9 @@
+// https://docs.flutter.dev/cookbook/forms/validation
+
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class FormWidget extends StatefulWidget {
   const FormWidget({super.key});
@@ -10,21 +15,40 @@ class FormWidget extends StatefulWidget {
 }
 
 class FormWidgetState extends State<FormWidget> {
+  /* 
+    Une clé globale identifie de manière unique des élèments dans toute
+    l'application.
+   */
   final _formKey = GlobalKey<FormState>();
+  TextEditingController contentController = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Form(
       key: _formKey,
       child: Column(
-        children: const <Widget>[
-          TextField(
-            obscureText: false,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Content',
-            ),
-          )
+        children: <Widget>[
+          TextFormField(
+            controller: contentController,
+            decoration: const InputDecoration(hintText: "What's new?"),
+          ),
+          ElevatedButton(
+              onPressed: (() {
+                if (_formKey.currentState!.validate()) {
+                  Future<http.Response> createAlbum(String title) {
+                    return http.post(
+                      Uri.parse('http://localhost:8082/posts'),
+                      headers: <String, String>{
+                        'Content-Type': 'application/json; charset=UTF-8',
+                      },
+                      body: jsonEncode(<String, String>{
+                        'content': contentController.text,
+                      }),
+                    );
+                  }
+                }
+              }),
+              child: const Icon(Icons.send))
         ],
       ),
     );
