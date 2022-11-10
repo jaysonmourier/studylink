114802569efb5e12f0ffcd00280fe94f7bbdecf2
Jayson Mourier <mourierjayson@protonmail.com>
2022-11-10 09:23:27 +0100

    updated build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
-cd studylink_api
-
-./mvnw clean package
-
-docker build . -t studylink_app
-
-cd ..
-
-docker-compose up -d
+DIR=studylink_api
+MVN=studylink_api/mvnw
+if [ -d "$DIR" ];
+then
+    if [ -f "$MVN" ];
+    then
+        if docker info > /dev/null 2>&1;
+        then
+            cd studylink_api
+            ./mvnw clean package
+            docker build . -t studylink_app
+            docker-compose up -d
+        else
+            echo "This script uses docker, and it isn't running - please start docker and try again!"
+        fi
+    else
+        echo "$MVN not found!"
+    fi
+    # check docker installation
+    # check container
+else
+	echo "$DIR directory does not exist."
+    echo "Please clone github.com/jaysonmourier/studylink"
+fi
