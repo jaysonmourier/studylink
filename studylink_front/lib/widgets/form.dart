e4e702dbefaced274d8557c1e300f77b94b00b23
Jayson Mourier <mourierjayson@protonmail.com>
2022-11-10 14:34:41 +0100

    added cors policy to the controller

--- studylink_front/lib/widgets/form.dart
+++ studylink_front/lib/widgets/form.dart
@@ -53,7 +53,7 @@ Future<Post> createPost(String content) async {
   final response = await http.post(
     Uri.parse('http://localhost:8081/posts'),
     headers: <String, String>{
-      'Content-Type': 'application/json; charset=UTF-8',
+      'Content-Type': 'application/json; charset=UTF-8'
     },
     body: jsonEncode(<String, String>{
       'content': content,
