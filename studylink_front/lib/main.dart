b0ce7d6c8ff0600ff7119ee8265b8410d38f72a6
Jayson Mourier <mourierjayson@protonmail.com>
2022-11-16 16:26:46 +0100

    centering form

--- studylink_front/lib/main.dart
+++ studylink_front/lib/main.dart
@@ -11,25 +11,23 @@ void main() {
 class Studylink extends StatelessWidget {
   const Studylink({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Studylink',
-      theme: ThemeData(
-        
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        body: SafeArea(
-          child: Container(
-            padding: EdgeInsets.all(20),
-            width: 500,
-            child: FormWidget(),
-            alignment: Alignment.center,
-          ),
+        title: 'Studylink',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
         ),
-      )
-    );
+        home: Scaffold(
+          body: SafeArea(
+              child: Center(
+              child: Container(
+              padding: EdgeInsets.all(20),
+              width: 500,
+              alignment: Alignment.center,
+              child: FormWidget(),
+            ),
+          )),
+        ));
   }
 }
