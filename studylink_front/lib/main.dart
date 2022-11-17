b626642adc0b62cfdedc1947df06ab697525a59e
Jayson Mourier <mourierjayson@protonmail.com>
2022-11-17 15:46:17 +0100

    get a post with its id

--- studylink_front/lib/main.dart
+++ studylink_front/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:beamer/beamer.dart';
 import 'package:flutter/material.dart';
+import 'package:studylink_web/pages/HomePage.dart';
 import 'package:studylink_web/pages/formPage.dart';
 import 'package:studylink_web/pages/postPage.dart';
 
@@ -10,17 +12,40 @@ void main() {
   runApp(const Studylink());
 }
 
-class Studylink extends StatelessWidget {
+class Studylink extends StatefulWidget {
   const Studylink({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      initialRoute: '/',
+  State<StatefulWidget> createState() {
+    return StudylinkState();
+  }
+}
+
+class StudylinkState extends State<Studylink> {
+  final routeDelegate = BeamerDelegate(
+    locationBuilder: RoutesLocationBuilder(
       routes: {
-        '/':(context) => const FormPage(),
-        '/post':(context) => const PostPage()
-      },
+        '/': (context, state, data) => const HomePage(),
+        '/posts': (context, state, data) => const FormPage(),
+
+        '/posts/:id': (context, state, data) {
+          final id = state.pathParameters['id']!;
+          return BeamPage(
+            key: ValueKey('post-$id'),
+            title: 'A simple post',
+            type: BeamPageType.scaleTransition,
+            child: PostPage(id: id)
+          );
+        }
+      } 
+      ),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routeInformationParser: BeamerParser(),
+      routerDelegate: routeDelegate,
     );
   }
 }
--- studylink_front/lib/pages/postPage.dart
+++ studylink_front/lib/pages/postPage.dart
@@ -2,25 +2,16 @@ import 'package:flutter/material.dart';
 
 import '../widgets/PostWidget.dart';
 
-class PostPage extends StatefulWidget {
-  const PostPage({super.key});
-
-  @override
-  State<StatefulWidget> createState() {
-    return PostPageState();
-  }
-}
-
-class PostPageState extends State<PostPage> {
-  String myurl = Uri.base.toString();
-  String? para1 = Uri.base.queryParameters["id"];
+class PostPage extends StatelessWidget {
+  String id;
+  PostPage({Key? key, required this.id}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    String test = para1 ?? "null";
-    return Scaffold(
-      body: PostWidget(),
-    );
+    return Container(
+      width: 150,
+      child: PostWidget(id: id),
+      );
   }
-
+  
 }
