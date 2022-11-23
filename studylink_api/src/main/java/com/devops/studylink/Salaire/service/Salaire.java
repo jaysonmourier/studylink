5d760f8e808a3932ff625b5f3cb993f2044c0486
Jayson Mourier <mourierjayson@protonmail.com>
2022-11-23 18:40:18 +0100

    add user id into salary table

--- studylink_api/src/main/java/com/devops/studylink/Salaire/service/Salaire.java
+++ studylink_api/src/main/java/com/devops/studylink/Salaire/service/Salaire.java
@@ -3,6 +3,7 @@ package com.devops.studylink.Salaire.service;
 import java.util.UUID;
 
 import com.devops.studylink.Salaire.repository.entities.SalaireEntity;
+import com.devops.studylink.User.service.User;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -16,6 +17,7 @@ import lombok.AllArgsConstructor;
 public class Salaire {
 
     private UUID id;
+    private UUID user;
     private Double grossSalary;
 
     public Salaire(SalaireEntity e) {
