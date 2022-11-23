786a01bc7aa3345833d0007899c365aedaeb8546
Jayson Mourier <mourierjayson@protonmail.com>
2022-11-23 17:55:53 +0100

    add salaire entity and refactor repository

---
+++ studylink_api/src/main/java/com/devops/studylink/Salaire/repository/entities/SalaireEntity.java
@@ -0,0 +1,31 @@
+package com.devops.studylink.Salaire.repository.entities;
+
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name="salaire")
+@Table(name="salaire")
+@Getter @Setter @NoArgsConstructor
+public class SalaireEntity {
+
+    @Id
+    @Type(type="uuid-char")
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name="id")
+    private UUID id;
+
+    @Column(name="name")
+    private Integer grossSalary;
+}
