b6c464ab3bc20ac257c54bcd8db519d80f6b5f8a
Jayson Mourier <mourierjayson@protonmail.com>
2023-01-05 13:14:20 +0100

    added navigation action to the button

--- studylink_front/lib/responsive/HomeHeroAppSectionMobile.dart
+++ studylink_front/lib/responsive/HomeHeroAppSectionMobile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:studylink_web/pages/formPage.dart';
 
 class HomeHeroAppSectionMobile extends StatelessWidget {
   const HomeHeroAppSectionMobile({super.key});
@@ -8,6 +9,8 @@ class HomeHeroAppSectionMobile extends StatelessWidget {
     return Padding(padding: const EdgeInsets.only(left: 0, top: 20, right: 0, bottom: 20), child: OutlinedButton(
       onPressed: () {
         debugPrint('Received click');
+        Navigator.of(context)
+          .push(MaterialPageRoute(builder: (_) => const FormPage()));
       },
       child: const Text('Access to the App'),
     ));
