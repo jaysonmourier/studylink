9b1fe739dcd754c410001c291f91d0c71f3bf80e
cdemiribel <clement.demiribel@gmail.com>
2023-01-05 17:16:21 +0100

    formulaire

--- studylink_api/src/main/java/com/devops/studylink/controllers/StatsController.java
+++ studylink_api/src/main/java/com/devops/studylink/controllers/StatsController.java
@@ -4,12 +4,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.devops.studylink.stats.dto.RecordDto;
 import com.devops.studylink.stats.service.Record;
 import com.devops.studylink.stats.service.StatsService;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
